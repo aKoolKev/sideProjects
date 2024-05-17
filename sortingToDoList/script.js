5f7e99f91d42ef8e3c04b1ac710b1f9a052fcc88
aKoolKev <kevindong0212@gmail.com>
2024-05-17 16:16:20 -0500

    Can delete individual task.

--- sortingToDoList/script.js
+++ sortingToDoList/script.js
@@ -56,56 +56,32 @@ function addTask(){
     //create new Task obj
     var newTask = new Task(taskEl.value, monthEl.value, dayEl.value, yearEl.value, importanceEl.value);
 
-
-    // //display task
-    // var listContainerEl = document.getElementById('listContainer');
-
-    // var liEl = document.createElement('li');
-
-    // //complete "button"
-    // var inputEl = document.createElement('input');
-    // inputEl.type = 'radio';
-    // liEl.appendChild(inputEl); //add radio button
-
-    // liEl.appendChild (document.createTextNode(newTask.name)); // add task name
-
-    // //create subfield under liEl to store the due date and importance
-    // var subfieldBoxEl = document.createElement('div');
-    // subfieldBoxEl.textContent = '[' + newTask.dueDateStr + '] (' + newTask.importance + ')';
-    // liEl.appendChild(subfieldBoxEl); //add subfield to li
-
-    // //delete task button
-    // buttonEl = document.createElement('button');
-    // buttonEl.textContent = 'X'; 
-    // subfieldBoxEl.appendChild(buttonEl); //add delete button
-
-    // listContainerEl.appendChild(liEl); //write li to ul
     
     //categorize Task by importance
     switch (importanceEl.value){
         case '1':
             importance1.push(newTask);
-            alert('i1: ' + importance1.length);
+            // alert('i1: ' + importance1.length);
             save('importance1', importance1);
             break;
         case '2':
             importance2.push(newTask);
-            alert('i2: '+importance2.length);
+            // alert('i2: '+importance2.length);
             save('importance2', importance2);
             break;
         case '3':
             importance3.push(newTask);
-            alert('i3: '+importance3.length);
+            // alert('i3: '+importance3.length);
             save('importance3', importance3);
             break;
         case '4':
             importance4.push(newTask);
-            alert('i4: '+importance4.length);
+            // alert('i4: '+importance4.length);
             save('importance4', importance4);
             break;
         case '5':
             importance5.push(newTask);
-            alert('i5: '+importance5.length);
+            // alert('i5: '+importance5.length);
             save('importance5', importance5);
             break;
         default:
@@ -124,11 +100,11 @@ function addTask(){
     yearEl.selectedIndex = 0;
     importanceEl.selectedIndex = 0;
 
-    print_taskList();
+    printTaskList();
 }
 
 //print all Task in arr
-function print_taskList(){
+function printTaskList(){
     var listContainerEl = document.getElementById('listContainer');
     listContainerEl.innerText = '';
 
@@ -153,6 +129,7 @@ function print_taskList(){
         let buttonEl = document.createElement('button');
         buttonEl.textContent = 'X'; 
         buttonEl.id = 'del_button';
+        buttonEl.addEventListener('click', deleteTask, false); // event to delete a task
         newTaskSubfield.appendChild(buttonEl); // add delete button
 
         listContainerEl.appendChild(newTask); //write li to ul
@@ -184,6 +161,10 @@ function clearTask(){
 
 //sort Task by due date (most urgent)
 function sortByDate(){
+    if (taskList.length < 1){
+        alert('No task to sort!');
+        return;
+    }
     //achieved with counting sort
 
     //get the max of the input list
@@ -214,7 +195,7 @@ function sortByDate(){
 
     taskList = sortedArr;
 
-    print_taskList();
+    printTaskList();
 }
 
 //function that prints out a given array
@@ -243,7 +224,8 @@ function printArr(arr){
             //delete task button
             buttonEl = document.createElement('button');
             buttonEl.textContent = 'X';
-            buttonEl.id = 'del_button'; 
+            buttonEl.id = 'del_button';
+            buttonEl.addEventListener('click', deleteTask, false); // event to delete a task 
 
             liSubEl.appendChild(buttonEl);
             liEl.appendChild(liSubEl);
@@ -253,8 +235,12 @@ function printArr(arr){
 }
 
 //sort Task by importance
-function sortByImportance(){
-    
+function sortByImportance(){ 
+    if (importance1.length+importance2.length+importance3.length+importance4.length+importance5.length < 1){
+        alert('No task to sort!');
+        return;
+    }
+
     var ulEl = document.getElementById('listContainer');
     ulEl.innerText = '';
     
@@ -338,36 +324,18 @@ window.onload = function() {
     defaultOption.value = 1;
 }
 
-function saveTaskList(){
-    let storeArrStr = JSON.stringify(taskList);
-    localStorage.setItem('taskList', storeArrStr);
-
-    storeArrStr = JSON.stringify(importance1);
-    localStorage.setItem('importance1', storeArrStr);
-
-    storeArrStr = JSON.stringify(importance2);
-    localStorage.setItem('importance2', storeArrStr);
-
-    storeArrStr = JSON.stringify(importance3);
-    localStorage.setItem('importance3', storeArrStr);
-
-    storeArrStr = JSON.stringify(importance4);
-    localStorage.setItem('importance4', storeArrStr);
-
-    storeArrStr = JSON.stringify(importance5);
-    localStorage.setItem('importance5', storeArrStr);
+//saves what was specified to local session
+function save(saveNameStr, thing2SaveObj){
+    const thing2Save_str = JSON.stringify(thing2SaveObj);
+    localStorage.setItem(saveNameStr, thing2Save_str);
 }
 
-function save(saveName, thing2Save ){
-    const thing2Save_str = JSON.stringify(thing2Save);
-    localStorage.setItem(saveName, thing2Save_str);
-}
-
+//load the Task 
 function loadTaskList(){
     //load the Task arr
     let retrievedArrStr = localStorage.getItem('taskList');
     taskList = JSON.parse(retrievedArrStr) || [];
-    print_taskList(); // print the array
+    printTaskList(); // print the array
 
     //load the importance arrays
     retrievedArrStr = localStorage.getItem('importance1');
@@ -384,4 +352,98 @@ function loadTaskList(){
 
     retrievedArrStr = localStorage.getItem('importance5');
     importance5 = JSON.parse(retrievedArrStr)|| [];
+}
+
+
+function deleteTask(e){
+    var targetEl = e.target;
+    var divEl = targetEl.parentNode;
+    var liEl = divEl.parentNode;
+    var ulEl = liEl.parentNode;
+
+    ulEl.removeChild(liEl); // only hids it, still lives in our taskList;
+
+    //EX: test[5/17/2024] (1)X
+
+    let taskName = liEl.textContent.split('['); // [test, 5/17/2024] (1)X]
+    
+    let dueDate = taskName[1].split(']'); // [5/17/2024, (1)X]
+
+    let importance = dueDate[1].split('('); // [   , 1)X]
+    importance = importance[1].split(')'); // [ 1, X]
+
+
+    //set all values
+    taskName = taskName[0];
+    dueDate = dueDate[0];
+    importance = importance[0];
+
+    //try to find and remove in taskList
+    for(let i=0; i<taskList.length; i++){
+        if (taskList[i].name === taskName && taskList[i].dueDateStr === dueDate && taskList[i].importance === importance){
+            // alert('found!');    
+            taskList.splice(i,1);
+            save('taskList',taskList);
+            break;
+        }
+    }
+    
+    // need to delete task from importance arrays as well
+    switch (importance)
+    {
+        case '1':{
+            for (let i=0; i<importance1.length; i++){
+                if (importance1[i].name === taskName && importance1[i].dueDateStr === dueDate){
+                    importance1.splice(i,1);
+                    save('importance1',importance1);
+                    break;
+                }
+            }
+            break;
+        }
+        case '2':{
+            for (let i=0; i<importance2.length; i++){
+                if (importance2[i].name === taskName && importance2[i].dueDateStr === dueDate){
+                    importance2.splice(i,1);
+                    save('importance2',importance2);
+                    break;
+                }
+            }
+            break;
+        }
+        case '3':{
+            for (let i=0; i<importance3.length; i++){
+                if (importance3[i].name === taskName && importance3[i].dueDateStr === dueDate){
+                    importance3.splice(i,1);
+                    save('importance3',importance3);
+                    break;
+                }
+            }
+            break;
+        }
+        case '4':{
+            for (let i=0; i<importance4.length; i++){
+                if (importance4[i].name === taskName && importance4[i].dueDateStr === dueDate){
+                    importance4.splice(i,1);
+                    save('importance4',importance4);
+                    break;
+                }
+            }
+            break;
+        }
+        case '5':{
+            for (let i=0; i<importance5.length; i++){
+                if (importance5[i].name === taskName && importance5[i].dueDateStr === dueDate){
+                    importance5.splice(i,1);
+                    save('importance5',importance5);
+                    break;
+                }
+            }
+            break;
+        }
+        default: {
+            alert('Deleting task from importance: Error on switch!');
+            break;
+        }
+    }
 }
